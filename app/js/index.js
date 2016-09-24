cb3a347bfc1c15c01016a91821685903a7148c30
Daniel Walker <plaxdan@gmail.com>
2016-09-24 15:32:29 -0600

    open chrome on start

--- app/js/index.js
+++ app/js/index.js
@@ -3,3 +3,13 @@ var app = document.getElementById('app');
 
 // Let's write some content to our web page...
 app.innerText = 'JavaScript is running!';
+
+console.log('This is a log');
+console.debug('This is a debug log');
+console.warn('This is a warn log');
+console.info('This is an info log');
+console.error('This is an error log');
+
+// alert('Here is an alert box. It has an OK button.')
+// confirm('This is a confirm dialog. It has OK and Cancel buttons.')
+// prompt('This is a prompt. It asks you for input.')
--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,33 @@
 'use strict';
 var gulp = require('gulp');
 var gulpUtil = require('gulp-util');
+var open = require('gulp-open');
+var os = require('os');
 var del = require('del');
 var webpack = require('webpack');
 var webpackConfig = require('./webpack.config.js');
 var WebpackDevServer = require('webpack-dev-server');
 
+var APP_PORT = 8080;
 var APP_PATH = './app';
 var BUILD_PATH = './build';
+
+var browser =  function() {
+  switch (os.platform()) {
+    case 'linux':
+      return 'google-chrome';
+      break;
+    case 'darwin':
+      return 'google chrome';
+      break;
+    case 'win32':
+      return 'chrome';
+      break;
+    default:
+      return 'google chrome'
+      break;
+  };
+}
 
 /**
  * This function adds different webpack config options
@@ -50,12 +70,27 @@ gulp.task('build', function(callback) {
   });
 });
 
-gulp.task('start', function() {
+/**
+ * Run the dev server.
+ */
+gulp.task('serve', function() {
   var config = getWebpackConfig();
   config.entry.app.unshift('webpack-dev-server/client?http://localhost:8080');
   var server = new WebpackDevServer(webpack(config), {
     contentBase: `${__dirname}/${APP_PATH}`,
     stats: {colors: true}
   });
-  server.listen(8080);
+  server.listen(APP_PORT);
+});
+
+/**
+ * Start the application in Chrome.
+ */
+gulp.task('start', ['serve'], function(){
+  var options = {
+    uri: `http://localhost:${APP_PORT}`,
+    app: browser()
+  };
+  gulp.src(__filename)
+    .pipe(open(options));
 });
